37c62dc1979a09523bdb3e7be1f1fc4c37040b71
yiiicc401 <ycen646@gmail.com>
2025-06-21 21:48:54 +0800

    調整 Flask 為 Render 運行環境

--- app.py
+++ app.py
@@ -60,4 +60,5 @@ def view():
     return render_template('view.html', data=data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
